416d7b070ce94d52ed3ce03d7086c77f8c651cc9
Dark-Code-Academy <darkacademy446@gmail.com>
2026-02-18 23:30:00 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,196 @@
+
+
+function ramadan () {
+
+    var phone_numper = "+96566473580";
+
+
+    var breakf = document.getElementById("ramadan-breakfast").value;
+    var suhoor = document.getElementById("ramadan-suhoor").value;
+    var drinks = document.getElementById("ramadan-drinks").value;
+    var salads = document.getElementById("ramadan-salads").value;
+    var soups = document.getElementById("ramadan-soups").value;
+    var desserts = document.getElementById("ramadan-desserts").value;
+
+
+    var url = "https://wa.me/" + phone_numper + "?text="
+    +"( يوم السبت )" + "%0a"
+    +"الفطار : " + breakf + "%0a"
+    +"السحور : " + suhoor + "%0a"
+    +"المشروبات : " + drinks + "%0a"
+    +"السلطة : " + salads + "%0a"
+    +"الشوربة : " + soups + "%0a"
+    +"الحلويات : " + desserts + "%0a";
+
+    
+    window.open(url, "_blanc").focus();
+}
+
+
+
+function ramadan1 () {
+
+    var phone_numper = "+96566473580";
+
+
+    var breakf = document.getElementById("ramadan1-breakfast").value;
+    var suhoor = document.getElementById("ramadan1-suhoor").value;
+    var drinks = document.getElementById("ramadan1-drinks").value;
+    var salads = document.getElementById("ramadan1-salads").value;
+    var soups = document.getElementById("ramadan1-soups").value;
+    var desserts = document.getElementById("ramadan1-desserts").value;
+
+
+    var url = "https://wa.me/" + phone_numper + "?text="
+    +"( يوم الاحد )" + "%0a"
+    +"الفطار : " + breakf + "%0a"
+    +"السحور : " + suhoor + "%0a"
+    +"المشروبات : " + drinks + "%0a"
+    +"السلطة : " + salads + "%0a"
+    +"الشوربة : " + soups + "%0a"
+    +"الحلويات : " + desserts + "%0a";
+    
+
+    
+    window.open(url, "_blanc").focus();
+}
+
+
+
+function ramadan2 () {
+
+    var phone_numper = "+96566473580";
+
+
+    var breakf = document.getElementById("ramadan2-breakfast").value;
+    var suhoor = document.getElementById("ramadan2-suhoor").value;
+    var drinks = document.getElementById("ramadan2-drinks").value;
+    var salads = document.getElementById("ramadan2-salads").value;
+    var soups = document.getElementById("ramadan2-soups").value;
+    var desserts = document.getElementById("ramadan2-desserts").value;
+
+
+    var url = "https://wa.me/" + phone_numper + "?text="
+    +"( يوم الاثنين )" + "%0a"
+    +"الفطار : " + breakf + "%0a"
+    +"السحور : " + suhoor + "%0a"
+    +"المشروبات : " + drinks + "%0a"
+    +"السلطة : " + salads + "%0a"
+    +"الشوربة : " + soups + "%0a"
+    +"الحلويات : " + desserts + "%0a";
+
+    
+    window.open(url, "_blanc").focus();
+}
+
+
+
+function ramadan3 () {
+
+    var phone_numper = "+96566473580";
+
+
+    var breakf = document.getElementById("ramadan3-breakfast").value;
+    var suhoor = document.getElementById("ramadan3-suhoor").value;
+    var drinks = document.getElementById("ramadan3-drinks").value;
+    var salads = document.getElementById("ramadan3-salads").value;
+    var soups = document.getElementById("ramadan3-soups").value;
+    var desserts = document.getElementById("ramadan3-desserts").value;
+
+
+    var url = "https://wa.me/" + phone_numper + "?text="
+    +"( يوم الثلاثاء )" + "%0a"
+    +"الفطار : " + breakf + "%0a"
+    +"السحور : " + suhoor + "%0a"
+    +"المشروبات : " + drinks + "%0a"
+    +"السلطة : " + salads + "%0a"
+    +"الشوربة : " + soups + "%0a"
+    +"الحلويات : " + desserts + "%0a";
+
+    
+    window.open(url, "_blanc").focus();
+}
+
+
+
+function ramadan4 () {
+
+    var phone_numper = "+96566473580";
+
+
+    var breakf = document.getElementById("ramadan4-breakfast").value;
+    var suhoor = document.getElementById("ramadan4-suhoor").value;
+    var drinks = document.getElementById("ramadan4-drinks").value;
+    var salads = document.getElementById("ramadan4-salads").value;
+    var soups = document.getElementById("ramadan4-soups").value;
+    var desserts = document.getElementById("ramadan4-desserts").value;
+
+
+    var url = "https://wa.me/" + phone_numper + "?text="
+    +"( يوم الاربعاء )" + "%0a"
+    +"الفطار : " + breakf + "%0a"
+    +"السحور : " + suhoor + "%0a"
+    +"المشروبات : " + drinks + "%0a"
+    +"السلطة : " + salads + "%0a"
+    +"الشوربة : " + soups + "%0a"
+    +"الحلويات : " + desserts + "%0a";
+
+    
+    window.open(url, "_blanc").focus();
+}
+
+
+
+function ramadan5 () {
+
+    var phone_numper = "+96566473580";
+
+
+    var breakf = document.getElementById("ramadan5-breakfast").value;
+    var suhoor = document.getElementById("ramadan5-suhoor").value;
+    var drinks = document.getElementById("ramadan5-drinks").value;
+    var salads = document.getElementById("ramadan5-salads").value;
+    var soups = document.getElementById("ramadan5-soups").value;
+    var desserts = document.getElementById("ramadan5-desserts").value;
+
+
+    var url = "https://wa.me/" + phone_numper + "?text="
+    +"( يوم الخميس )" + "%0a"
+    +"الفطار : " + breakf + "%0a"
+    +"السحور : " + suhoor + "%0a"
+    +"المشروبات : " + drinks + "%0a"
+    +"السلطة : " + salads + "%0a"
+    +"الشوربة : " + soups + "%0a"
+    +"الحلويات : " + desserts + "%0a";
+
+    
+    window.open(url, "_blanc").focus();
+}
+
+
+
+function ramadan6 () {
+
+    var phone_numper = "+96566473580";
+
+
+    var breakf = document.getElementById("ramadan6-breakfast").value;
+    var suhoor = document.getElementById("ramadan6-suhoor").value;
+    var drinks = document.getElementById("ramadan6-drinks").value;
+    var salads = document.getElementById("ramadan6-salads").value;
+    var soups = document.getElementById("ramadan6-soups").value;
+    var desserts = document.getElementById("ramadan6-desserts").value;
+
+
+    var url = "https://wa.me/" + phone_numper + "?text="
+    +"( يوم الجمعة )" + "%0a"
+    +"الفطار : " + breakf + "%0a"
+    +"السحور : " + suhoor + "%0a"
+    +"المشروبات : " + drinks + "%0a"
+    +"السلطة : " + salads + "%0a"
+    +"الشوربة : " + soups + "%0a"
+    +"الحلويات : " + desserts + "%0a";
+
+    
+    window.open(url, "_blanc").focus();
+}
